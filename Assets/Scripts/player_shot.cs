6055df4be19cdd20bdcd27079448fa67593ab2e9
ArielGoulart <31768080+ArielGoulart@users.noreply.github.com>
2020-04-14 11:24:51 -0300

    shot&mine
    Adicionei função de atirar e deixar uma mina. Por enquanto não existe colisão, nem destruição dos objetos.
    
    Espaço para Atirar
    E para deixar a mina
    W A S D ou Setas para a Movimentação

---
+++ Assets/Scripts/player_shot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_shot : MonoBehaviour
+{
+    public GameObject shot;
+    public GameObject mine;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(shot, player.transform.position, player.transform.rotation);
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(mine, player.transform.position, player.transform.rotation);
+        }
+    }
+}
