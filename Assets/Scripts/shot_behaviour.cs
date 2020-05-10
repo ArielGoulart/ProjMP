13f8b47217de821e31455815505b41361a0294af
DominikCG <44614079+DominikCG@users.noreply.github.com>
2020-05-09 22:43:23 -0300

    meh
    algumas modificaçoes e embelezamentos nada muito elaborado

--- Assets/Scripts/shot_behaviour.cs
+++ Assets/Scripts/shot_behaviour.cs
@@ -8,9 +8,8 @@ public class shot_behaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     // Update is called once per frame
     void Update()
     {
