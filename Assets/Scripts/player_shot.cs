1611c797a857277de55222cc39850a6807ea0f6e
ArielGoulart <31768080+ArielGoulart@users.noreply.github.com>
2020-06-18 23:18:25 -0300

    SFX Sounds
    Adicionei o sons de tiro de torpedo e da mina sendo deixada. É isso

--- Assets/Scripts/player_shot.cs
+++ Assets/Scripts/player_shot.cs
@@ -6,42 +6,48 @@ using UnityEngine;
 namespace Photon.Shot
 {
 
-public class player_shot : MonoBehaviourPun
-{
-    [SerializeField] private GameObject shot;
-    [SerializeField] private GameObject mine;
-    [SerializeField] private GameObject player;
-    [SerializeField] private Transform aux_mine;
-    [SerializeField] private Transform aux_shot;
-    // Start is called before the first frame update
-    void Start()
+    public class player_shot : MonoBehaviourPun
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(photonView.IsMine)
+        [SerializeField] private GameObject shot;
+        [SerializeField] private GameObject mine;
+        [SerializeField] private GameObject player;
+        [SerializeField] private Transform aux_mine;
+        [SerializeField] private Transform aux_shot;
+        public AudioClip torpedo_shot;
+        public AudioClip deploy_mine;
+        AudioSource audioSource;
+        // Start is called before the first frame update
+        void Start()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (photonView.IsMine)
             {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
                     Fire();
+                    audioSource.PlayOneShot(torpedo_shot, 0.0f);
+
+                }
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    FireMine();
+                    audioSource.PlayOneShot(deploy_mine, 0.0f);
+                }
             }
 
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                    FireMine();
-               }
         }
 
-    }
-       
         private void Fire()
-         {
+        {
             PhotonNetwork.Instantiate(shot.name, aux_shot.transform.position, player.transform.rotation);
         }
-      
+
         private void FireMine()
         {
             PhotonNetwork.Instantiate(mine.name, aux_mine.transform.position, player.transform.rotation);
